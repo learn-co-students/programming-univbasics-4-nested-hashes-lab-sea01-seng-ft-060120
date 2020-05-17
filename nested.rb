da428000c4fe0e9bc93312fac3998bf277c17321
brave-ping-4201 <h_yazie@yahoo.com>
2020-05-17 02:37:22 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
+   programmer_hash[:grace_hopper]
 
 end
 
@@ -36,7 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languages] = "C"
 
 end
 
@@ -65,6 +67,7 @@ def adding_matz
 #   :languages => ["LISP", "C"]
 # }
 # return the entire updated hash
+ 
 
 	programmer_hash = 
  		{
@@ -81,6 +84,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
+   programmer_hash[:yukihiro_matsumoto] = {
+  known_for: "Ruby",
+  languages: ["LISP", "C"]
+  }
+   programmer_hash
+    
 
 end
 
@@ -103,6 +112,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer_hash
 
 end
 
@@ -125,5 +136,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    programmer_hash
 
 end
